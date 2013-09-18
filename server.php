f91610e054df98d066155acee641978653178b3e
Patamon <patamongo@gmail.com>
2013-09-18 10:59:36 +0800

    initial commit

---
+++ server.php
@@ -0,0 +1,16 @@
+<?php
+
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+$uri = urldecode($uri);
+
+$paths = require __DIR__.'/bootstrap/paths.php';
+
+$requested = $paths['public'].$uri;
+
+if ($uri !== '/' and file_exists($requested))
+{
+	return false;
+}
+
+require_once $paths['public'].'/index.php';
