f7a952c5b9c8406d968808ef3e4d6e8fcf3cee95
Fyiorb <fyiorb@gmail.com>
2013-10-01 20:33:28 +0800

    markdown support

--- app/models/BaseModel.php
+++ app/models/BaseModel.php
@@ -1,9 +1,16 @@
 <?php
 
+use \Michelf\Markdown;
+
 class BaseModel extends Eloquent {
 
 	public static function validate($data)
 	{
 		return Validator::make($data, static::$rules);
+	}
+
+	public static function markdown($text)
+	{
+		return Markdown::defaultTransform($text);
 	}
 }
