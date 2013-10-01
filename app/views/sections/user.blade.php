5bd67281691b46d20b13f94e5304217ab4d8ffd5
Fyiorb <fyiorb@gmail.com>
2013-10-01 10:43:19 +0800

    fix views structure

--- app/views/sections/user.blade.php
+++
@@ -1,5 +0,0 @@
-<ul class="nice-tabs">
-	<li class="{{ (Request::segment(3)) ? '' : 'active' }}"><a href="{{ URL::to('u/' . 'Suzy') }}">{{ Lang::get('page.user_info') }}</a></li>
-	<li class="{{ (Request::segment(3) == 'topics') ? 'active' : '' }}"><a href="{{ URL::to('u/' . 'Suzy' . '/topics') }}">{{ Lang::get('page.user_topics') }}</a></li>
-	<li><a href="{{ URL::to('u/' . 'Suzy' . '/followers') }}">{{ Lang::get('page.user_follower') }}</a></li>
-</ul>
