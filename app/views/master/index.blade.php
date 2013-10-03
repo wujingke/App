52a90f610d90f8088d949a8a1cea775969f018e0
Fyiorb <fyiorb@gmail.com>
2013-10-03 16:27:45 +0800

    ready to setting config.yml file

--- app/views/master/index.blade.php
+++ app/views/master/index.blade.php
@@ -5,7 +5,11 @@
 @stop
 
 @section('sidebar')
-  <div class="nice-box">
-    
-  </div>
+    <div class="nice-box">
+        <ul class="nice-tabs">
+            <li><a href="{{ URL::to('~master/topic') }}">{{ Lang::get('page.topic') }}</a></li>
+            <li><a href="{{ URL::to('~master/node') }}">{{ Lang::get('page.node') }}</a></li>
+            <li><a href="{{ URL::to('~master/reply') }}">{{ Lang::get('page.reply') }}</a></li>
+        </ul>
+    </div>
 @stop
