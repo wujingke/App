e2f46e4bf9f362aca3973c1fcfbb453623849292
Fyiorb <fyiorb@gmail.com>
2013-10-03 17:03:55 +0800

    Symfony Yaml

--- app/controllers/MasterController.php
+++ app/controllers/MasterController.php
@@ -1,15 +1,23 @@
 <?php
 
+use Symfony\Component\Yaml\Yaml;
+
 class MasterController extends BaseController {
 
 	public function index()
 	{
-		return View::make('master.index');
+		//return View::make('master.index');
+		return Yaml::parse(app_path() . '/config/app.yml');
 	}
 
 	public function nodeStore()
 	{
 
+	}
+
+	private function isAdmin()
+	{
+		var_dump(Yaml::parse(app_path() . '/config/app.yml'));
 	}
 
 }
