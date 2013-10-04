b0b948a176cc1e77d5a9a811e55cc0424e68eff1
Fyiorb <fyiorb@gmail.com>
2013-10-04 10:05:08 +0800

    add HTMLPurifier

--- app/config/app.php
+++ app/config/app.php
@@ -113,6 +113,7 @@ return array(
 		'Illuminate\Validation\ValidationServiceProvider',
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
+		'Mews\Purifier\PurifierServiceProvider',
 
 	),
 
@@ -178,6 +179,7 @@ return array(
 		'URL'             => 'Illuminate\Support\Facades\URL',
 		'Validator'       => 'Illuminate\Support\Facades\Validator',
 		'View'            => 'Illuminate\Support\Facades\View',
+		'Purifier'        => 'Mews\Purifier\Facades\Purifier',
 
 	),
 
