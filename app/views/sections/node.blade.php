9945781a96124352069e666b61213ed1f98b678c
Fyiorb <fyiorb@gmail.com>
2013-10-09 15:41:40 +0800

    to scss

--- app/views/sections/node.blade.php
+++ app/views/sections/node.blade.php
@@ -1,4 +1,4 @@
-<ul class="nodes">
+<ul class="nice-tabs">
 	@foreach($nodes as $node)
 	<li class="{{ (Request::segment(2) == $node->pretty) ? 'active' : 'nil' }}">
 		<a href="{{ URL::to('node/' . $node->pretty) }}">{{ $node->name }}<span>{{ $node->topics->count() }}</span></a>
