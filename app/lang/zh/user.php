0e8d5c092edb1cc6081163e211fb92d915853b94
Fyiorb <fyiorb@gmail.com>
2013-10-13 14:36:40 +0800

    fix layouts.master

--- app/lang/zh/user.php
+++ app/lang/zh/user.php
@@ -2,10 +2,17 @@
 
 return array(
 
-	"followers" => "跟随者",
+    "followers" => "跟随者",
 
-	"following" => "关注",
+    "following" => "关注",
 
-	"topics"    => "话题",
+    "topics"    => "话题",
 
+    "settings"  => "设置",
+
+    "signup"    => "注册",
+
+    "login"     => "登录",
+
+    "logout"    => "退出",
 );
--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -1,48 +1,50 @@
 <!doctype html>
 <html>
 <head>
-  <meta charset="UTF-8">
-  <title>Title</title>
-  <link rel="stylesheet" type="text/css" href="http://me:9292/assets/application.css">
-  <script type="text/javascript" src="http://me:9292/assets/application.js"></script>
+    <meta charset="UTF-8">
+    <title>{{ Session::has('title') ? Session::get('title') : Lang::get('site.title') }}</title>
+    <link media="all" type="text/css" rel="stylesheet" href="http://me:9292/assets/application.css">
+    <script src="http://me:9292/assets/application.js"></script>
 </head>
 <body>
-  <div class="wrapper">
-    <div class="nav nav-bar top-fixed">
-      <div class="row">
-        <a href="{{ URL::to('/') }}" class="logo"></a>
-        <ul class="io columns tiles">
-          @if(Auth::check())
-            <li><a href="{{ URL::to('u/' . Auth::user()->username) }}">{{ Auth::user()->username }}</a></li>
-            <li><a href="{{ URL::to('settings') }}">{{ Lang::get('page.settings') }}</a></li>
-            <li><a href="{{ URL::to('logout') }}">{{ Lang::get('page.logout') }}</a></li>
-          @else
-            <li><a href="{{ URL::to('signup') }}">{{ Lang::get('page.signup') }}</a></li>
-            <li class="login"><a href="{{ URL::to('login') }}">{{ Lang::get('page.login') }}</a></li>
-          @endif
-        </ul>
-      </div>
-    </div>
-    <div class="master">
-      <div class="row">
-        <div class="eight columns">
-          <div class="nice-box">
-            @yield('app')
-          </div>
+    <div class="wrapper">
+        <div class="nav-bar top-fixed">
+            <div class="row">
+                <div class="logo">
+                    <a href="{{ URL::route('home') }}"></a>
+                </div>
+                <ul class="io columns tiles">
+                    @if(Auth::check())
+                    <li><a href="{{ URL::to('u/' . Auth::user()->username) }}">{{ Auth::user()->username }}</a></li>
+                    <li><a href="{{ URL::to('settings') }}">{{ Lang::get('user.settings') }}</a></li>
+                    <li><a href="{{ URL::to('logout') }}">{{ Lang::get('user.logout') }}</a></li>
+                    @else
+                    <li><a href="{{ URL::to('signup') }}">{{ Lang::get('user.signup') }}</a></li>
+                    <li class="login"><a href="{{ URL::to('login') }}">{{ Lang::get('user.login') }}</a></li>
+                    @endif
+                </ul>
+            </div>
         </div>
-        <div class="four columns">
-          @yield('sidebar')
+        <div class="master">
+            <div class="row">
+                <div class="eight columns">
+                    <div class="nice-box">
+                        @yield('app')
+                    </div>
+                </div>
+                <div class="four columns">
+                    @yield('sidebar')
+                </div>
+            </div>
         </div>
-      </div>
+        <div class="footer">
+            <div class="row">
+                <p>&copy; 2013 Fyiorb · All rights reserved.</p>
+            </div>
+        </div>
     </div>
-    <div class="footer">
-      <div class="row">
-        <p>&copy; 2013 ampou.com · All rights reserved.</p>
-      </div>
-    </div>
-  </div>
-  @unless(Auth::check())
-    @include('sections.login')
-  @endunless
+    @unless(Auth::check())
+        @include('sections.login')
+    @endunless
 </body>
 </html>
